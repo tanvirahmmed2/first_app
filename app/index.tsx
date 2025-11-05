1b7132d9593bcea6f3fe8b8e9f787360bc9fb0ca
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-05 19:07:04 +0600

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,9 @@
-import {  Text,  TouchableOpacity, View } from "react-native";
+import {  Text,  View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Creating first movie app</Text>
-      <TouchableOpacity>
-        <Text style={{
-          backgroundColor: 'black',
-          color: 'white',
-          width: 100,
-          textAlign: 'center'
-        }}>Press me</Text>
-      </TouchableOpacity>
+    <View className="flex-1 justify-center">
+      <Text className="bg-black text-white text-center">Hello</Text>
     </View>
   );
 }
