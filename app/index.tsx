7abc90276551dc5d81ab8c771c145db7571ae333
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-05 19:11:51 +0600

    update

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import {  Text,  View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center">
-      <Text className="bg-black text-white text-center">Hello</Text>
+    <View className="flex-1 justify-center items-center bg-secondary">
+      <Text className="text-sky-500 text-2xl font-semibold">Welcome</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#030014',
+        secondary:'#151312'
+      }
+    },
   },
   plugins: [],
 };
