00268c69cf594bdf42d6d559105b7b300a5c02a8
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-08 01:10:05 +0600

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { router } from "expo-router";
 import {   Image, ScrollView, View } from "react-native";
 
 export default function Index() {
@@ -12,7 +13,7 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '100%', paddingBottom:10}}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
         <View className="flex-1 mt-5">
-          <SearchBar/>
+          <SearchBar onPress={()=> router.push("/search")} placeholder="search for a movie" />
         </View>
       </ScrollView>
 
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,13 +1,16 @@
 import { Image, TextInput, View} from 'react-native'
 import React from 'react'
 import { icons } from '@/constants/icons'
-
-const SearchBar = () => {
+interface Props{
+    placeholder:string,
+    onPress?:()=>void
+}
+const SearchBar = ({placeholder, onPress}:Props) => {
   return (
     <View className='flex-row items-center bg-black rounded-full p-4'>
       <Image source={icons.search} className='size-5' resizeMode='contain' />
-      <TextInput onPress={()=>{}}
-        placeholder='search'
+      <TextInput onPress={onPress}
+        placeholder={placeholder}
         value='search'
         onChangeText={()=>{}}
         className='text-white'
