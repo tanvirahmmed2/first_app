262fa5fd363393d4d46a0e2c9cbc435c987936da
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-07 23:50:27 +0600

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
 
+import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import {   Image, View } from "react-native";
+import {   Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
     
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
+      <ScrollView>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+      </ScrollView>
 
     </View>
   );
