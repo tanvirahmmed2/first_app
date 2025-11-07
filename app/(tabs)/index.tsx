582ec98aa626ce0e9cefe21d1bdb3f4ea60efa2d
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-08 00:00:24 +0600

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import {   Image, ScrollView, View } from "react-native";
@@ -10,6 +11,9 @@ export default function Index() {
       <Image source={images.bg} className="absolute w-full z-0"/>
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '100%', paddingBottom:10}}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        <View className="flex-1 mt-5">
+          <SearchBar/>
+        </View>
       </ScrollView>
 
     </View>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,12 @@
-import { View, ScrollView, Image } from 'react-native'
+import { Image, TextInput, View} from 'react-native'
 import React from 'react'
 import { icons } from '@/constants/icons'
 
 const SearchBar = () => {
   return (
-    <View>
-       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '100%', paddingBottom:10}}>
-              <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-            </ScrollView>
-      
+    <View className='flex-row items-center bg-black rounded-full p-4'>
+      <Image source={icons.search} className='size-5' resizeMode='contain' />
+      <TextInput />
     </View>
   )
 }
