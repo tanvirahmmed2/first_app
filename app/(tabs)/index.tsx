d84e83db5084e89b639fdb22f66948db09eae1b4
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-07 23:45:26 +0600

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
 
-import {  Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {  Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
+    <View className="flex-1 justify-center items-center">
       <Text className="text-sky-500 text-6xl font-semibold hover:opacity-20">Welcome</Text>
       
-    </SafeAreaView>
+    </View>
   );
 }
