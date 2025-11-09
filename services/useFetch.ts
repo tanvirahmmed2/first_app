7bab16f361e3cd569f5c90b7c1ae2e72e1a53b11
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-09 12:31:06 +0600

    update

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 const useFetch=<T>(fetchFunction: ()=> Promise<T>, autoFetch= true)=>{
+    const [data, setData]= useState<T | null> (null)
+    const [loading, setLoading]= useState(false)
+    const [error, setError]= useState<Error | null> (null)
 
 
 }
